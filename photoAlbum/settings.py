af307ac781d6813420d9d9841b6cfe771f0ea053
Mrat Pann Khine <mpk@gmail.com>
2023-06-30 20:37:40 +0800

    add staticfiles

--- photoAlbum/settings.py
+++ photoAlbum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+rsoz%79qdubfp5i)xn1dj+ll_3zkugdd+rrctc+kn2!mke9w4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
